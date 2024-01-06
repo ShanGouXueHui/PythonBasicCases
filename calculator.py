b5536d9d4427d21f20f3a873b380c39bdc2e392d
19820813985@139.com <19820813985@139.com>
2024-01-06 17:59:42 +0800

    rebase

--- calculator.py
+++ calculator.py
@@ -71,10 +71,10 @@ logging 使用非常简单，使用 basicConfig() 方法就能满足基本的使
 '''
 
 #设置全局logging配置、以及日志格式
-logging.basicConfig(filename="run.log", 
+logging.basicConfig(filename="./PythonCases/run.log", 
                    filemode="w",                    
                    format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
-                   datefmt="%d-%M-%Y %H:%M:%S", 
+                   datefmt="%D-%M-%Y %H:%M:%S", 
                    level=logging.INFO,
                    encoding='utf-8')
 
